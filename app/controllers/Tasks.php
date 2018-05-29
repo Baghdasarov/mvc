d0ccbc0c85bfda0e7ecf9e19498b32fab83d419d
Norayr <noro9424@gmail.com>
2018-05-30 00:11:05 +0400

    Image resize on upload

--- app/controllers/Tasks.php
+++ app/controllers/Tasks.php
@@ -47,29 +47,57 @@ class Tasks extends Controller {
     public function store() {
         $this->authCheck();
         $data = $_POST;
-
         if(isset($_FILES['image']['name']) && !empty($_FILES['image']['name'])){
-
-            $image_info = getimagesize($_FILES["image"]["tmp_name"]);
-            $image_width = $image_info[0];
-            $image_height = $image_info[1];
-//            dd('width - ' . $image_width . ', height - ' . $image_height);
             $name = $_FILES['image']['name'];
             $unique = uniqid() . '.';
             $target_dir = "uploads/";
             $target_file = $target_dir . basename($name);
             $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-            $extensions_arr = array("jpg","jpeg","png","gif");
+            $extensions_arr = array("jpg","png","gif");
             if( in_array($imageFileType,$extensions_arr) ){
-                move_uploaded_file($_FILES['image']['tmp_name'],$target_dir.$unique.$imageFileType);
-                $data['image'] = '/'.$target_dir.$unique.$imageFileType;
+                $image = '/'.$target_dir.$unique.$imageFileType;
+                $data['image'] = $image;
+                $this->model->createTask('tasks', $data);
+                $image = $_FILES['image']['tmp_name'];
+                $image_info = getimagesize($image);
+                $image_width = $image_info[0];
+                $image_height = $image_info[1];
+                if (($image_width < 321 && $image_height < 241) || ($image_width < 241 && $image_height < 321)) {
+                    move_uploaded_file($_FILES['image']['tmp_name'],$target_dir.$unique.$imageFileType);
+                    header('location:/Task/index');
+                } else {
+                    $original_img = imagecreatefromjpeg($image);
+                    if ($image_width > $image_height) {
+                        if (($image_width / 320) > ($image_height / 240)) {
+                            $thumb_w = 320;
+                            $thumb_h = $image_height / ($image_width / 320);
+                        } else {
+                            $thumb_h = 240;
+                            $thumb_w = $image_width / ($image_height / 240);
+                        }
+                    } else {
+                        if (($image_width / 320) > ($image_height / 240)) {
+                            $thumb_w = 240;
+                            $thumb_h = $image_height / ($image_width / 240);
+                        } else {
+                            $thumb_h = 320;
+                            $thumb_w = $image_width / ($image_height / 320);
+                        }
+                    }
+                    $thumb_img = imagecreatetruecolor($thumb_w, $thumb_h);
+                    imagecopyresampled($thumb_img, $original_img,
+                        0, 0,
+                        0, 0,
+                        $thumb_w, $thumb_h,
+                        $image_width, $image_height);
+                    imagejpeg($thumb_img,  $target_dir.$unique.$imageFileType);
+                    imagedestroy($thumb_img);
+                    header('location:/Task/index');
+                }
             } else {
                 return false;
             }
         }
-        $this->model->createTask('tasks', $data);
-
-        header('location:/Task/index');
     }
 
     public function update() {
